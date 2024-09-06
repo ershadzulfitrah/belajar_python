2fa16127cc7c0c15dffdcb187e26464a30e22701
Ershad Zulfitrah Hans Dako <96755721+ershadzulfitrah@users.noreply.github.com>
2024-09-07 06:58:36 +0700

    start learn python

---
+++ input.py
@@ -0,0 +1,16 @@
+# input_nama = input("Nama kamu siapa? ")
+#
+# print(input_nama)
+#
+# a = 20
+# b = 30
+# angkamu = input('masukan angkamu ')
+# c = (a+b)/int(angkamu)
+# print(c)
+
+saldo_awal = input('masukan saldo awal: ')
+deposit = input('deposit: ')
+
+saldo_akhir = int(saldo_awal) + int(deposit)
+
+print("saldo terbaru anda: " + str(saldo_akhir))
---
+++ main.py
@@ -0,0 +1,9 @@
+nama = 'Ershad Zulfitrah Hans Dako'
+umur = 27
+greets = 'Hallo my name is '
+sebut = 'umur saya: '
+tinggi_badan = 183.5
+
+print(greets+nama)
+print( sebut+str(umur))
+print('tinggi badan saya adalah: '+str(tinggi_badan) +' cm')
